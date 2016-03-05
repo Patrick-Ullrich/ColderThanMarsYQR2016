021cb0e009da29ed32541b948551f2d9b26cefb0
Patrick Ullrich <patrick.ullrich93@gmail.com>
2016-03-05 15:25:42 -0600

    Page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,10 +24,55 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/2006', {
+        templateUrl: 'views/2006.html',
+        controller: 'MainCtrl',
+        controllerAs: 'mail'
+      })
+      .when('/2007', {
+        templateUrl: 'views/2007.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2008', {
+        templateUrl: 'views/2008.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2009', {
+        templateUrl: 'views/2009.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2010', {
+        templateUrl: 'views/2010.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2011', {
+        templateUrl: 'views/2011.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2012', {
+        templateUrl: 'views/2012.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2013', {
+        templateUrl: 'views/2013.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2014', {
+        templateUrl: 'views/2014.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/2015', {
+        templateUrl: 'views/2015.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       })
       .otherwise({
         redirectTo: '/'
---
+++ app/scripts/data.js
@@ -0,0 +1,20 @@
+var skByYear = [
+	{
+		"year": "2006",
+		"emission": [
+			{"energy": 54700},
+			{"industrial": 827},
+			{"agriculture": 12000},
+			{"waste": 840}
+		]
+	},
+	{
+		"year": "2007",
+		"emission": [
+			{"energy": 56400},
+			{"industrial": 793},
+			{"agriculture": 12000},
+			{"waste": 850}
+		]
+	}
+];
