9c081161caa11b120cb6ff860c6f510f5209a1b8
Maha El Meseery <elmeseem@uregina.ca>
2016-03-05 18:49:51 -0600

    got d3 working!

--- app/scripts/directives/customgraph.js
+++ app/scripts/directives/customgraph.js
@@ -1,22 +1,12 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name colderThanMarsYqr2016App
+ * @ngdoc directive
+ * @name colderThanMarsYqr2016App.directive:customGraph
  * @description
- * # colderThanMarsYqr2016App
- *
- * Main module of the application.
+ * # customGraph
  */
-angular
-  .module('colderThanMarsYqr2016App', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
+angular.module('colderThanMarsYqr2016App')
   .directive('custom-graph', [function() {
 
 
@@ -37,7 +27,7 @@ angular
       scope: {
 
       },
-      link: function(scope, element, attrs)
+      controller: function(scope, element, attrs)
        {
 var  htmlelement=element[0];
 
@@ -320,64 +310,10 @@ var line = d3.svg.line()
 
 
 
-});  /// d3 .csv ... 
+});  /// d3 .csv ...
 
 
-
-       },
-       controller: function($scope) {
 
        }
      };
-  }])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/1990.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/1990', {
-        templateUrl: 'views/1990.html',
-        controller: 'MainCtrl',
-        controllerAs: 'mail'
-      })
-      .when('/1995', {
-        templateUrl: 'views/1995.html',
-        controller: 'MainCtrl',
-        controllerAs: 'mail'
-      })
-      .when('/2000', {
-        templateUrl: 'views/2000.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/2005', {
-        templateUrl: 'views/2005.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/2010', {
-        templateUrl: 'views/2010.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/2011', {
-        templateUrl: 'views/2011.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/2012', {
-        templateUrl: 'views/2012.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/2013', {
-        templateUrl: 'views/2013.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  }]);
